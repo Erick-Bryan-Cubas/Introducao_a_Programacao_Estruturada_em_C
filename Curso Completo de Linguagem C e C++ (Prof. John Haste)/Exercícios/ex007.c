b94b2561c8f4e13c4790759159a07eea70707ed0
Erick Bryan Cubas <thecubas@outlook.com>
2023-04-08 19:54:59 -0300

    commit

---
+++ Curso Completo de Linguagem C e C++ (Prof. John Haste)/Exercícios/ex007.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<windows.h>
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    /* Crie um algoritmo que imprima os números de 10 até 0
+    de forma regressiva. (Usando o While)*/
+
+    
+
+return 0;
+
+}
