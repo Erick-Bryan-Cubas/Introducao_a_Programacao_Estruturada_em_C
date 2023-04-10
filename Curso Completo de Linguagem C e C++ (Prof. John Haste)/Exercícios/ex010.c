c730f76bed4dbe0d944edb2798f657e296887781
Erick Bryan Cubas <thecubas@outlook.com>
2023-04-10 03:18:06 -0300

    commit

---
+++ Curso Completo de Linguagem C e C++ (Prof. John Haste)/Exercícios/ex010.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<windows.h>
+#define BR 3
+
+int main(){
+    //Para usar acentos
+    setlocate(LC_ALL, "");
+    //Para titular o programa
+    SetConsoleTitle("EXERCÍCIO 10");
+
+    //Enunciado
+    printf("Crie um  algoritmo que leia 3 valores para um vetor de 3 posições"
+    " e depois calcule a média dos valores acessando o vetor");
+
+    int vetor[BR], i;
+    
+
+return 0;
+
+}
+
