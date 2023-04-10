d4f66bc4505b6706b10af1942d74089111d3b6cb
Erick Bryan Cubas <thecubas@outlook.com>
2023-04-10 03:52:58 -0300

    commit

---
+++ Curso Completo de Linguagem C e C++ (Prof. John Haste)/Exercícios/ex010_for.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void main (){
+
+    int tamanho;
+    printf("Qual será o tamanho do vetor?");
+    scanf("%d", &tamanho);
+
+    float vetor[tamanho], soma;
+    int i;
+    for(i = 0; i < tamanho; i++){
+        printf("Digite um valor:");
+        scanf("%f", &vetor[i]);
+        soma+=vetor[i];
+    }
+    for(i=0; i<tamanho; i++){
+        printf("\n vetor[%d] = %.2f", i, vetor[i]);
+    }
+    printf("\n A média dos valores é: %.2f", soma/tamanho);
+}
