3ff33d125d3cfbdbc057fc887a9b2d53de7d7228
Erick Bryan Cubas <thecubas@outlook.com>
2023-04-25 04:58:09 -0300

    commit

---
+++ Aprendizado/Material Didático/Programas/helloworld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+//void é uma função que não necessita de retorno
+void main(){
+        setlocale(LC_ALL,"");
+        printf("Olá, mundo!");
+}
---
+++ Aprendizado/Material Didático/Programas/helloworld_with_return.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main é a função inicial que exige retorno
+int main(){
+        printf("Ola, mundo!");
+        return 0;
+}
